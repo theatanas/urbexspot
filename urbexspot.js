9fcd1e7b4c597464bc367a6d5c1f049bdc3ed283
atns dmtrv <Inga@iMAC-Inga.fritz.box>
2015-11-16 16:18:57 +0100

    Installing packages
    Prepared packages
    Created structure
    Beginning the implementation of Google Maps

--- urbexspot.js
+++ urbexspot.js
@@ -1,23 +1,50 @@
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
+    Template.registerForm.events({
+        'submit form' : function(event, template) {
+            event.preventDefault();
+            var emailVal = template.find("#register-email").value,
+                passwordVal = template.find("#register-password").value;
+            
+            Accounts.createUser({
+                email: emailVal,
+                password: passwordVal
+            })
+        }
+    })
 
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
-  });
+    Template.loginForm.events({
+        'submit form' : function(event, template) {
+            event.preventDefault();
+            var emailVal = template.find("#login-email").value,
+                passwordVal = template.find("#login-password").value;
+                Meteor.loginWithPassword(emailVal, passwordVal);
+        }
+    })
+
+    Template.dashboard.events({
+        'click #logout': function(event) {
+            event.preventDefault();
+            Meteor.logout();
+        }
+    })
+
+    // Session.setDefault('counter', 0);
+    // Template.hello.helpers({
+    //     counter: function() {
+    //         return Session.get('counter');
+    //     }
+    // });
+
+    // Template.hello.events({
+    //     'click button': function() {
+    //         Session.set('counter', Session.get('counter') + 1);
+    //     }
+    // });
 }
 
 if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
+    Meteor.startup(function() {
+        // code to run on server at startup
+    });
 }
